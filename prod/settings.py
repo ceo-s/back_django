41e11e96cbb1472dd35650c8975af39f5397f879
ceo-s <alex_s_2002@mail.ru>
2023-05-29 01:43:50 +0300

    Workflow should work now

--- prod/settings.py
+++ prod/settings.py
@@ -14,11 +14,14 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-# load_dotenv(".env.dev")
-# load_dotenv(".env.dev.db")
-
-# load_dotenv(".env.prod")
-# load_dotenv(".env.prod.db")
+if os.getenv("ENV_VERSION") == "dev":
+    load_dotenv(".env.dev")
+    load_dotenv(".env.dev.db")
+elif os.getenv("ENV_VERSION") == "test":
+    load_dotenv(".env.test")
+elif os.getenv("ENV_VERSION") == "prod":
+    load_dotenv(".env.prod")
+    load_dotenv(".env.prod.db")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
